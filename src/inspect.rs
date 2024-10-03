bd5d18444409024d0bd75e39460b286453a0b2ed
Findiglay <dev@grantfindlay.com>
2024-10-03 08:52:48 +0100

    Display tool multiplier

--- src/inspect.rs
+++ src/inspect.rs
@@ -21,7 +21,7 @@ impl Miner {
             let tool_data = self.rpc_client.get_account_data(&tool_address).await.unwrap();
             let tool = Tool::try_from_bytes(&tool_data).unwrap();
 
-            print_tool_info(tool_address, tool.authority, tool.durability, tool.multiplier);
+            print_tool_info(tool.asset, tool.authority, amount_u64_to_f64(tool.durability), tool.multiplier);
         } else if let Some(tool) = args.tool {
             let asset_address = Pubkey::from_str(&tool).unwrap();
             let asset_data = self.rpc_client.get_account_data(&asset_address).await.unwrap();
@@ -29,7 +29,8 @@ impl Miner {
             let attributes_plugin = asset.plugin_list.attributes.unwrap();
             let durability_attr = attributes_plugin.attributes.attribute_list.iter().find(|attr| attr.key == "durability");
             let multiplier_attr = attributes_plugin.attributes.attribute_list.iter().find(|attr| attr.key == "multiplier");
-            let durability = durability_attr.unwrap().value.parse::<u64>().unwrap();
+            
+            let durability = durability_attr.unwrap().value.parse::<f64>().unwrap();
             let multiplier = multiplier_attr.unwrap().value.parse::<u64>().unwrap();
 
             print_tool_info(asset_address, asset.base.owner, durability, multiplier);
@@ -37,12 +38,12 @@ impl Miner {
     }
 }
 
-fn print_tool_info(mint: Pubkey, owner: Pubkey, durability: u64, multiplier: u64) {
+fn print_tool_info(mint: Pubkey, owner: Pubkey, durability: f64, multiplier: u64) {
     println!(
         "\n\nTool Inspected: {} \nOwner: {} \nDurability: {} \nMultiplier: {}x",
         mint,
         owner,
-        amount_u64_to_f64(durability),
-        multiplier as f64 / 100.0,
+        durability,
+        1.0 + (multiplier as f64 / 100.0),
     );
 }
